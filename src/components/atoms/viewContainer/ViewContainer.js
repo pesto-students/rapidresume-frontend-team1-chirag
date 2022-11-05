50ecdc2a808e2c6f86a58c2726dce95b2c0e243c
Sagar Khatri <dev.sagarkhatri@gmail.com>
2022-11-05 10:41:33 +0530

    deleted non useful libraries

--- src/components/atoms/viewContainer/ViewContainer.js
+++ src/components/atoms/viewContainer/ViewContainer.js
@@ -1,10 +1,8 @@
 import React from "react";
-import { View } from "@react-pdf/renderer";
+
 
 const ViewContainer = ({ platform, children, style, className }) => {
-  if (platform === "pdf") {
-    return <View style={style}>{children}</View>;
-  }
+  
 
   return (
     <div className={className} style={style}>
